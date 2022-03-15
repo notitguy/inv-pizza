b66a459175303e869b82e56f9508ce4d8a81466e
Aziz <84846799+erkinoov20@users.noreply.github.com>
2022-03-15 16:47:35 +0500

    Update reservation.php

--- reservation.php
+++ reservation.php
@@ -63,27 +63,27 @@
                 <div class="col-md-4 select-container">
                   <label>Time</label>
                   <select name="time" id="time">
-                    <option value="11">11:00</option>
-                    <option value="11">11:30</option>
-                    <option value="11">12:00</option>
-                    <option value="11">12:30</option>
-                    <option value="11">13:00</option>
-                    <option value="11">13:30</option>
-                    <option value="11">14:00</option>
-                    <option value="11">14:30</option>
-                    <option value="11">15:00</option>
-                    <option value="11">15:30</option>
-                    <option value="11">16:00</option>
-                    <option value="11">16:30</option>
-                    <option value="11">17:00</option>
-                    <option value="11">17:30</option>
-                    <option value="11">18:00</option>
-                    <option value="11">18:30</option>
-                    <option value="11">19:00</option>
-                    <option value="11">19:30</option>
-                    <option value="11">20:00</option>
-                    <option value="11">20:30</option>
-                    <option value="11">21:00</option>
+                    <option value="11:00">11:00</option>
+                    <option value="11:30">11:30</option>
+                    <option value="12:00">12:00</option>
+                    <option value="12:30">12:30</option>
+                    <option value="13:00">13:00</option>
+                    <option value="13:30">13:30</option>
+                    <option value="14:00">14:00</option>
+                    <option value="14:30">14:30</option>
+                    <option value="15:00">15:00</option>
+                    <option value="15:30">15:30</option>
+                    <option value="16:00">16:00</option>
+                    <option value="16:30">16:30</option>
+                    <option value="17:00">17:00</option>
+                    <option value="17:30">17:30</option>
+                    <option value="18:00">18:00</option>
+                    <option value="18:30">18:30</option>
+                    <option value="19:00">19:00</option>
+                    <option value="19:30">19:30</option>
+                    <option value="20:00">20:00</option>
+                    <option value="20:30">20:30</option>
+                    <option value="21:00">21:00</option>
                   </select>
                   <span>
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
