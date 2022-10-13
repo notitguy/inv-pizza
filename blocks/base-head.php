16f94e7410c6d1cf45fa9a99c682c6df9f6ea399
Notitguy Office <kelvin77@seznam.cz>
2022-10-13 14:05:05 +0200

    add trailing slashes to hreflang

--- blocks/base-head.php
+++ blocks/base-head.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8" />
 <meta content="width=device-width, initial-scale=1" name="viewport" />
 <link rel="alternate" href="https://pizza.invitaly.cz" hreflang="cs" />
-<link rel="alternate" href="https://pizza.invitaly.cz/en" hreflang="en" />
+<link rel="alternate" href="https://pizza.invitaly.cz/en/" hreflang="en" />
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;400;600;700&display=swap" rel="stylesheet">
